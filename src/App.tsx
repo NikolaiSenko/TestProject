67e5e0d28b115deba9994cb903e15cd347e00567
Senko Nikolai <kolya.senko333@gmail.com>
2023-02-02 16:09:58 +0300

    lessons 7 and 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import Counter from "./components/Counter";
-import "./index.scss"
+import { Route, Routes, Link } from "react-router-dom";
+import { Suspense, useContext } from "react";
+import "./styles/index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { UseTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
-    return (
-        <div className="app">
-            <Counter/>
-        </div>
-    );
+  const { theme, toggleTheme } = UseTheme();
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
