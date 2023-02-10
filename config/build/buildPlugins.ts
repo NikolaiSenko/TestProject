ffd29dbe8fba8839e77856ce681992236a8b6d4e
Senko Nikolai <kolya.senko333@gmail.com>
2023-02-10 14:35:58 +0300

    lesson 16

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import path from "path";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import  ReactRefreshPlugin  from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({
   paths,
@@ -20,5 +21,7 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshPlugin(),
   ];
 }
