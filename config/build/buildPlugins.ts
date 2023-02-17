83c15cdd753937ad930d8786917d479c3cdab2fc
Senko Nikolai <kolya.senko333@gmail.com>
2023-02-17 16:28:41 +0300

    refactor build plugins for production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,28 +1,32 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
-import { type BuildOptions } from './types/config'
+import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export function buildPlugins ({
+export function buildPlugins({
   paths,
-  isDev
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
-      template: paths.html
+      template: paths.html,
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
+      __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false })
   ]
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin())
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+  }
+
+  return plugins
 }
